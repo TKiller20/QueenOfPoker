14b84f47ee373b0cb0d36aef3a20326119901c27
TKiller20 <taydonrees@gmail.com>
2025-11-09 10:07:55 -0700

    initial project creation

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Simple echo program: read a line from the user and print it back.
+Console.Write("Enter some text: ");
+string? input = Console.ReadLine();
+if (string.IsNullOrEmpty(input))
+{
+	Console.WriteLine("No input provided.");
+}
+else
+{
+	Console.WriteLine($"You entered: {input}");
+}
